d368cf427912aeb1b2c505dcd7fa76f202ac5f66
rivaldynaiborhu <rivaldynaiborhu@gmail.com>
2022-02-02 12:23:23 +0700

    updated project, add cv and more

--- apps.js
+++ apps.js
@@ -1,3 +1,5 @@
+
+// Hamburger button, mobile, and tablet
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul'); 
 const header = document.querySelector('.header.container');
@@ -7,16 +9,18 @@ hamburger.addEventListener('click', ()=>{
     mobile_menu.classList.toggle('active');
 })
 
+//header animation
 document.addEventListener('scroll', ()=>{
     var scroll_position = window.scrollY;
     if(scroll_position >250){
-        header.style.backgroundColor = "#29323c"
+        header.style.backgroundColor = "#1f2833"
     }
     else{
         header.style.backgroundColor = "transparent"
     }
 })
 
+//paralax
 let bg = document.getElementById("bg");
 let moon = document.getElementById("moon");
 let mouuntain = document.getElementById("mountain");
